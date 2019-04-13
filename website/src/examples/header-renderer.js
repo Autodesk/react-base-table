a96dfb2a90bdfa75fab50d353c0c4da409aecc8c
Neo Nie <nihgwu@live.com>
2019-04-14 00:03:04 +0800

    [website] update examples

--- website/src/examples/header-renderer.js
+++ website/src/examples/header-renderer.js
@@ -4,11 +4,42 @@ const data = generateData(columns, 200)
 const Row = styled.div`
   display: flex;
   align-items: center;
+  width: 100%;
   height: 100%;
-  padding: 0 15px;
 `
 
-const headerRenderer = ({ columns }) => <Row>This is a custom Header</Row>
+const CustomHeader = styled.div`
+  display: flex;
+  flex-grow: 1;
+  padding: 0 7.5px;
+`
+
+columns[0].frozen = Column.FrozenDirection.LEFT
+columns[1].frozen = Column.FrozenDirection.LEFT
+columns[2].frozen = Column.FrozenDirection.RIGHT
+
+const headerRenderer = ({ cells, columns }) => {
+  // frozen table's header
+  if (columns.every(x => x.frozen)) return cells
+
+  // scrollalbe table's header, as there are placeholders for the frozen cells
+  // we have to keep them to make sure the custom content display in the right palce
+  const leftPlaceholders = []
+  const rightPlaceholders = []
+  columns.forEach((column, idx) => {
+    if (column.frozen === Column.FrozenDirection.RIGHT)
+      rightPlaceholders.push(cells[idx])
+    else if (column.frozen) leftPlaceholders.push(cells[idx])
+  })
+
+  return (
+    <Row>
+      {leftPlaceholders}
+      <CustomHeader>This is a custom Header</CustomHeader>
+      {rightPlaceholders}
+    </Row>
+  )
+}
 
 export default () => (
   <Table fixed columns={columns} data={data} headerRenderer={headerRenderer} />
--- website/src/examples/multi-header.js
+++ website/src/examples/multi-header.js
@@ -1,21 +1,60 @@
 const columns = generateColumns(10)
 const data = generateData(columns, 200)
 
-const Row = styled.div`
+const GroupCell = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   height: 100%;
-  padding: 0 15px;
+
+  &:not(:last-child) {
+    border-right: 1px solid lightgray;
+  }
 `
 
+columns[0].frozen = Column.FrozenDirection.LEFT
+columns[1].frozen = Column.FrozenDirection.LEFT
+columns[2].frozen = Column.FrozenDirection.RIGHT
+
 const headerRenderer = ({ cells, columns, headerIndex }) => {
-  if (headerIndex === 0) return <Row>This is a custom Header</Row>
-  if (headerIndex === 1) return cells.slice(0, 5)
-  return cells
+  if (headerIndex === 2) return cells
+
+  const groupCells = []
+  let width = 0
+  let idx = 0
+
+  columns.forEach((column, columnIndex) => {
+    // if there are frozen columns, there will be some placeholders for the frozen cells
+    if (column[Table.PlaceholderKey]) groupCells.push(cells[columnIndex])
+    else {
+      width += cells[columnIndex].props.style.width
+      idx++
+
+      const nextColumn = columns[columnIndex + 1]
+      if (
+        columnIndex === columns.length - 1 ||
+        nextColumn[Table.PlaceholderKey] ||
+        idx === 3 - headerIndex
+      ) {
+        groupCells.push(
+          <GroupCell
+            key={`header-group-cell-${column.key}`}
+            style={{ ...cells[columnIndex].props.style, width }}
+          >
+            Group width {width}
+          </GroupCell>
+        )
+        width = 0
+        idx = 0
+      }
+    }
+  })
+  return groupCells
 }
 
 export default () => (
   <Table
+    fixed
     columns={columns}
     data={data}
     headerHeight={[30, 40, 50]}
